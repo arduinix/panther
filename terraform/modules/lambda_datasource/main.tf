027d840bd43073637a78e7b080b07f3df7352775
arduinix <arduinix88@gmail.com>
2024-04-30 12:14:53 -0600

    terraform module for lambda datasources

---
+++ terraform/modules/lambda_datasource/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    }
+  }
+}
+
+locals {
+  app_env_fn_name = "${var.app_name}-${var.env}"
+}
+
+data "archive_file" "this" {
+  for_each    = var.functions
+  output_path = "${path.root}/out/${each.key}.zip"
+  source_dir  = lookup(each.value, "source_dir", "${path.root}/../lambda")
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "this" {
+  for_each         = var.functions
+  function_name    = "${local.app_env_fn_name}-${each.key}"
+  runtime          = lookup(each.value, "runtime", var.default_runtime)
+  handler          = "${var.dist_sub_dir}/${each.key}.handler"
+  role             = aws_iam_role.this[each.key].arn
+  filename         = var.publish ? data.archive_file.this[each.key].output_path : null
+  source_code_hash = var.publish ? data.archive_file.this[each.key].output_base64sha256 : null
+  publish          = var.publish
+  timeout          = each.value.timeout
+  layers           = lookup(each.value, "lambda_layer_arn", null)
+  environment {
+    variables = merge(var.default_env_vars, lookup(each.value, "env_vars", {}))
+  }
+}
+
+resource "aws_appsync_datasource" "this" {
+  for_each         = var.functions
+  api_id           = var.appsync_id
+  name             = each.key
+  type             = "AWS_LAMBDA"
+  service_role_arn = var.appsync_role_arn
+
+  lambda_config {
+    function_arn = aws_lambda_function.this[each.key].arn
+  }
+}
+
+resource "aws_appsync_resolver" "this" {
+  # for_each          = var.functions
+  count             = var.create_resolver ? length(var.functions) : 0
+  api_id            = var.appsync_id
+  field             = each.key
+  type              = each.value.resolver_type
+  data_source       = aws_appsync_datasource.this[each.key].name
+  request_template  = file(lookup(each.value, "request_template", "${path.root}/../api/lambda.default.request.vtl"))
+  response_template = file(lookup(each.value, "response_template", "${path.root}/../api/lambda.default.response.vtl"))
+
+  caching_config {
+    caching_keys = lookup(each.value, "caching_keys", ["$context.identity"])
+    ttl          = 900
+  }
+}
---
+++ terraform/modules/lambda_datasource/variables.tf
@@ -0,0 +1,82 @@
+variable "app_name" {
+  type        = string
+  description = "The app name."
+}
+
+variable "appsync_id" {
+  type        = string
+  description = "The appsync id."
+}
+
+variable "appsync_role_arn" {
+  type        = string
+  description = "The appsync service role arn."
+}
+
+variable "caching_keys" {
+  type        = list(string)
+  default     = ["$context.identity"]
+  description = "A list of caching keys for the resolver."
+}
+
+variable "env" {
+  type        = string
+  description = "The environment name for the microservice."
+}
+
+variable "env_vars" {
+  type        = map(string)
+  default     = {}
+  description = "Additional lambda environment variables to merge with the default."
+}
+
+variable "functions" {
+  description = "The function configuration"
+}
+
+variable "region" {
+  type        = string
+  description = "The region the infrastructure is deployed to."
+}
+
+variable "subdir" {
+  type        = string
+  description = "The subdirectory of the lambda function."
+  default     = null
+}
+
+
+variable "default_statements" {
+  default     = []
+  description = "The lambda iam policy document statements."
+}
+
+variable "dist_sub_dir" {
+  description = "The directory containing the lambda function handlers"
+  type        = string
+  default     = "dist"
+}
+
+variable "default_env_vars" {
+  type        = map(string)
+  default     = {}
+  description = "Default lambda environment variables to apply to all lambdas"
+}
+
+variable "default_runtime" {
+  type        = string
+  default     = "nodejs20.x"
+  description = "The default lambda runtime"
+}
+
+variable "publish" {
+  type        = bool
+  default     = false
+  description = "Whether to publish the lambda function"
+}
+
+variable "create_resolver" {
+  type        = bool
+  default     = true
+  description = "Whether to create a resolver for the lambda function"
+}
